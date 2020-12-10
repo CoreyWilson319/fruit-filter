0fcf76dd5494550bc6a0c2bfb9b7c64028fe2985
Your Name <you@example.com>
2020-12-10 12:11:03 -0500

    [FruitsContainer] Add class component and import components

--- src/components/List.js
+++ src/components/List.js
@@ -1,17 +1,19 @@
-import react, { Component } from 'react';
+import React, { Component } from 'react';
 
 class List extends Component {
     constructor(props) {
         super()
         this.state = {
-            
+
         };
     }
 
     render() {
         return(
             <div>
-
+                <ul>
+                    {/* List will go here. */}
+                </ul>
             </div>
         );
     }
