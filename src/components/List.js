75f09e96782e36e1507458213ae38006d4e3a3bb
Your Name <you@example.com>
2020-12-10 11:42:22 -0500

    List component added code.

---
+++ src/components/List.js
@@ -0,0 +1,20 @@
+import react, { Component } from 'react';
+
+class List extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+            
+        };
+    }
+
+    render() {
+        return(
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default List
